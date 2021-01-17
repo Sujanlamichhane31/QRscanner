1b0dd07020e4036586e255d90b34eae951436c60
Sujan <sujanlamichhane742@example.com>
2021-01-18 00:00:23 +0545

    Final Product

--- lib/generatepage.dart
+++ lib/generatepage.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
-import 'package:qrscanner/model.dart';
 
 class GeneratePage extends StatefulWidget {
   @override
@@ -10,16 +9,9 @@ class GeneratePage extends StatefulWidget {
 
 class _GeneratePageState extends State<GeneratePage> {
   
-  GlobalKey<FormState> _key = GlobalKey();
-  
-  Products dummyData;
+  String dummyData;
   TextEditingController qrTextController = TextEditingController();
-  TextEditingController productname = TextEditingController();
-  TextEditingController vendorname = TextEditingController();
-  TextEditingController sellingprice= TextEditingController();
-  TextEditingController discountprice = TextEditingController();
   
-  String pname, vname, sp, dp;
   
   @override
   Widget build(BuildContext context) {
@@ -28,117 +20,39 @@ class _GeneratePageState extends State<GeneratePage> {
         title:  Text("Generate QR code"),
       ),
       body: SingleChildScrollView(
-        child: Form(
-          key: _key,
-          // ignore: deprecated_member_use
-          autovalidate: true,
-          child: Container(
-            child: Column(
-              children: [
-                Row(
-                children: [
-                  Text("Vendor Name : "),
-                  SizedBox(width: 12.0,),
-                  Container(
-                    height: 30.0,
-                    width: 250.0,
-                    child: TextFormField(
-                      autofocus:  true,
-                      controller: vendorname,
-                      decoration: InputDecoration(
-                      ),
-                      onSaved: (String value){
-                        vname = value;
-                      },
-                    ),
-                  )
-                ],
+        child: Container(
+          child: Column(
+            children: [
+              Text("Enter the data "),
+              SizedBox(width: 12.0,),
+              Container(
+                height: 30.0,
+                width: 200.0,
+                child: TextFormField(
+                  autofocus:  true,
+                  controller: qrTextController,
+                  decoration: InputDecoration(
+                  ),
+                ),
               ),
-                SizedBox(height: 25.0),
-                Row(
-                  children: [
-                    Text("Product Name : "),
-                    SizedBox(width: 12.0,),
-                    Container(
-                      height: 30.0,
-                      width: 250.0,
-                      child: TextFormField(
-                        autofocus:  true,
-                        controller: productname,
-                        decoration: InputDecoration(
-                        ),
-                        onSaved: (String value){
-                          pname = value;
-                        },
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(height: 25.0),
-                Row(
-                  children: [
-                    Text("Selling Price : "),
-                    SizedBox(width: 12.0,),
-                    Container(
-                      height: 30.0,
-                      width: 250.0,
-                      child: TextFormField(
-                        keyboardType: TextInputType.phone,
-                        autofocus:  true,
-                        controller: sellingprice,
-                        decoration: InputDecoration(
-                        ),
-                        onSaved: (String value){
-                          sp = value;
-                        },
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(height: 25.0),
-                Row(
-                  children: [
-                    Text("Discount Price : "),
-                    SizedBox(width: 12.0,),
-                    Container(
-                      height: 30.0,
-                      width: 250.0,
-                      child: TextFormField(
-                        keyboardType: TextInputType.phone,
-                        autofocus:  true,
-                        controller: discountprice,
-                        decoration: InputDecoration(
-                        ),
-                        onSaved: (String value){
-                          dp = value;
-                        },
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(height: 25.0),
-              RaisedButton(
-                  child: Text("submit"),
-                  onPressed: (){
-                    setState(() {
-                      dummyData = Products()== ""?
-                      null :
-                      Products(
-                        vendorname: vendorname.text,
-                        productname: productname.text,
-                        sellingprice: sellingprice.text,
-                        discountprice: discountprice.text
-                      );
-                    });
-                  }),
-                (dummyData == null)
-                ?Center(child: Text("Enter some text to display qr code..."))
-                  :QrImage(
-                data: vendorname.text,
-              gapless: true,
-              )
-              ],
-            ),
+             
+            RaisedButton(
+                child: Text("submit"),
+                onPressed: (){
+                  setState(() {
+                    dummyData = qrTextController.text == ""
+                    ? null :
+                    qrTextController.text;
+                  });
+                }),
+              (dummyData == null)
+                  ? Center(child: Text("enter some text to display qr code..."))
+                  : QrImage(
+               
+                data: dummyData,
+                gapless: true,
+              ),
+            ],
           ),
         ),
       )
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:qrscanner/generatepage.dart';
 import 'package:qrscanner/scanpage.dart';
